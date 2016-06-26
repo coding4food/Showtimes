8d857df2787d089cc2190bda9aea1736a327170d
odin <chudishko@gmail.com>
2016-06-26 20:28:27 +0300

    Typo fix

--- Showtimes.Domain/Movie.cs
+++ Showtimes.Domain/Movie.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Showtimes.Domain
 {
-    public public class Movie
+    public class Movie
     {
         public int MovieId { get; set; }
         public string Title { get; set; }

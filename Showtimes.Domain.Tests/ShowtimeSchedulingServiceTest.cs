54f4c28c97830fd184914f7a9791d918b21fc60c
odin <chudishko@gmail.com>
2016-06-28 17:17:17 +0300

    Added some argument validation to showtime scheduling

--- Showtimes.Domain.Tests/ShowtimeSchedulingServiceTest.cs
+++ Showtimes.Domain.Tests/ShowtimeSchedulingServiceTest.cs
@@ -2,7 +2,9 @@
 using System.Linq;
 using System.Data.Entity;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace Showtimes.Domain.Tests
 {
@@ -64,6 +66,69 @@ namespace Showtimes.Domain.Tests
             Assert.IsTrue(actual.Any());
             Assert.AreEqual(sessionTimes.Length, actual.Count());
             CollectionAssert.AreEquivalent(sessionTimes, actual.Select(s => s.SessionTime).ToArray());
+        }
+
+        [TestMethod]
+        public void ScheduleShowtime_Throws_For_Invalid_MovieId()
+        {
+            var mockMovies = Mock.Of<IRepository<Movie>>(r => r.FindAsync(It.IsAny<int>()) == Task.FromResult<Movie>(null));
+            var uow = Mock.Of<IUnitOfWork>(u => u.Movies == mockMovies);
+
+            var sut = new ShowtimesSchedulingService(uow);
+
+            Func<Task> act = async () => await sut.ScheduleShowtimes(1000, 1000, new[] { DateTime.Now });
+
+            act.ShouldThrow<ArgumentException>().Where(ex => ex.Message.Contains("movieId"));
+        }
+
+        [TestMethod]
+        public void ScheduleShowtime_Throws_For_Invalid_MovieTheaterId()
+        {
+            int movieId = 1000;
+
+            var mockMovies = Mock.Of<IRepository<Movie>>(r => r.FindAsync(movieId) == Task.FromResult<Movie>(new Movie { MovieId = movieId, Title = "AAA" }));
+            var mockTheatres = Mock.Of<IRepository<MovieTheater>>(r => r.FindAsync(It.IsAny<int>()) == Task.FromResult<MovieTheater>(null));
+            var uow = Mock.Of<IUnitOfWork>(u => u.Movies == mockMovies && u.MovieTheatres == mockTheatres);
+
+            var sut = new ShowtimesSchedulingService(uow);
+
+            Func<Task> act = async () => await sut.ScheduleShowtimes(1000, movieId, new[] { DateTime.Now });
+
+            act.ShouldThrow<ArgumentException>().Where(ex => ex.Message.Contains("movieTheaterId"));
+        }
+
+        [TestMethod]
+        public void ScheduleShowtime_Throws_For_Null_SessionTimes()
+        {
+            int movieId = 1000;
+            int movieTheaterId = 1000;
+
+            var mockMovies = Mock.Of<IRepository<Movie>>(r => r.FindAsync(movieId) == Task.FromResult<Movie>(new Movie { MovieId = movieId, Title = "AAA" }));
+            var mockTheatres = Mock.Of<IRepository<MovieTheater>>(r => r.FindAsync(movieTheaterId) == Task.FromResult<MovieTheater>(new MovieTheater { MovieTheaterId = movieTheaterId, Name = "BBB" }));
+            var uow = Mock.Of<IUnitOfWork>(u => u.Movies == mockMovies && u.MovieTheatres == mockTheatres);
+
+            var sut = new ShowtimesSchedulingService(uow);
+
+            Func<Task> act = async () => await sut.ScheduleShowtimes(movieTheaterId, movieId, null);
+
+            act.ShouldThrow<ArgumentNullException>().Where(ex => ex.Message.Contains("sessionTimes"));
+        }
+
+        [TestMethod]
+        public void ScheduleShowtime_Throws_For_Empty_SessionTimes()
+        {
+            int movieId = 1000;
+            int movieTheaterId = 1000;
+
+            var mockMovies = Mock.Of<IRepository<Movie>>(r => r.FindAsync(movieId) == Task.FromResult<Movie>(new Movie { MovieId = movieId, Title = "AAA" }));
+            var mockTheatres = Mock.Of<IRepository<MovieTheater>>(r => r.FindAsync(movieTheaterId) == Task.FromResult<MovieTheater>(new MovieTheater { MovieTheaterId = movieTheaterId, Name = "BBB" }));
+            var uow = Mock.Of<IUnitOfWork>(u => u.Movies == mockMovies && u.MovieTheatres == mockTheatres);
+
+            var sut = new ShowtimesSchedulingService(uow);
+
+            Func<Task> act = async () => await sut.ScheduleShowtimes(movieTheaterId, movieId, Enumerable.Empty<DateTime>());
+
+            act.ShouldThrow<ArgumentException>().Where(ex => ex.Message.Contains("sessionTimes"));
         }
     }
 }
--- Showtimes.Domain/ShowtimesSchedulingService.cs
+++ Showtimes.Domain/ShowtimesSchedulingService.cs
@@ -15,16 +15,38 @@ namespace Showtimes.Domain
             this.unitOfWork = unitOfWork;
         }
 
-        public Task ScheduleShowtimes(int movieTheaterId, int movieId, IEnumerable<DateTime> sessionTimes)
+        public async Task ScheduleShowtimes(int movieTheaterId, int movieId, IEnumerable<DateTime> sessionTimes)
         {
             // TODO add some validation
+
+            if (sessionTimes == null)
+            {
+                throw new ArgumentNullException(nameof(sessionTimes));
+            }
+
+            if (!sessionTimes.Any())
+            {
+                // тут ArgumentException, потому что при таком списке сеансов невозможно получить дату расписания
+                // если бы дата и сеансы передавались отдельно, можно было бы удалить все сеансы при пустом списке на этот день
+                throw new ArgumentException(nameof(sessionTimes));
+            }
+
+            if ((await this.unitOfWork.Movies.FindAsync(movieId)) == null)
+            {
+                throw new ArgumentException(nameof(movieId));
+            }
+
+            if ((await this.unitOfWork.MovieTheatres.FindAsync(movieTheaterId)) == null)
+            {
+                throw new ArgumentException(nameof(movieTheaterId));
+            }
 
             foreach (var showtime in sessionTimes.Select(time => new Showtimes(movieTheaterId, movieId, time)))
             {
                 this.unitOfWork.Showtimes.Insert(showtime);
             }
 
-            return this.unitOfWork.SaveAsync();
+            await this.unitOfWork.SaveAsync();
         }
 
         public Task<IEnumerable<Showtimes>> ShowtimesForADate(DateTime date)

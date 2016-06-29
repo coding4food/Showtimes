bcb5a756b6ad4835f41eb7f849c0fe97d14e1260
odin <chudishko@gmail.com>
2016-06-29 13:50:23 +0300

    Added index page to schedules controller with showtimes table

---
+++ Showtimes/Controllers/ScheduleController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Showtimes.Domain;
+using Showtimes.Models;
+using System.Threading.Tasks;
+
+namespace Showtimes.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private IUnitOfWork unitOfWork = new UnitOfWork();
+
+        // GET: Schedule
+        public async Task<ActionResult> Index(DateTime? date)
+        {
+            var d = date ?? DateTime.Today.AddSeconds(1);
+
+            var model = new ShowtimesList
+            {
+                Date = d,
+                MovieTheatres = await unitOfWork.MovieTheatres.GetAllAsync(),
+                Movies = await unitOfWork.Movies.GetAllAsync(),
+                Showtimes = await unitOfWork.Showtimes.GetAllByDateAsync(d)
+            };
+
+            return View(model);
+        }
+
+        // GET: Schedule/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Schedule/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Schedule/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schedule/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Schedule/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Schedule/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Schedule/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Showtimes/Models/ShowtimesList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Showtimes.Domain;
+
+namespace Showtimes.Models
+{
+    public class ShowtimesList
+    {
+        [DataType(DataType.Date)]
+        [UIHint("Date")]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; } = DateTime.Today;
+        public IEnumerable<MovieTheater> MovieTheatres { get; set; }
+        public IEnumerable<Movie> Movies { get; set; }
+        public IEnumerable<Showtimes.Domain.Showtimes> Showtimes { get; set; }
+    }
+}
